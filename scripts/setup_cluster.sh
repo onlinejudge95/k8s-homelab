2e72c5d08ddfb24863ba9cb4c0db3f654fa9569f
Mayank Pathak <onlinejudge95@gmail.com>
2026-01-17 11:57:44 +0530

    Implements copilot review

--- scripts/setup_cluster.sh
+++ scripts/setup_cluster.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # Ensure the script is run with appropriate privileges
 if [ "$EUID" -ne 0 ]; then
     echo "This script must be run with sudo or as root."
@@ -11,6 +13,6 @@ sudo swapoff -a
 
 # Disable swap in /etc/fstab to persist across reboots
 echo "Disabling swap in /etc/fstab..."
-sudo sed -i '/[[:space:]]swap[[:space:]]/ s/^\(.*\)$/#\1/g' /etc/fstab
+sudo sed -i.bak '/[[:space:]]swap[[:space:]]/ s/^\(.*\)$/#\1/g' /etc/fstab
 
 echo "Swap disabled successfully."
